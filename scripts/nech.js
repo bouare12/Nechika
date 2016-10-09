a092e1dbf27c313bbe9081e14f223530761e7a69
bouare12 <bandiougoubouare@yahoo.fr>
2016-10-09 21:50:29 +0200

    coo

---
+++ scripts/nech.js
@@ -0,0 +1,5 @@
+//<![CDATA[
+jQuery(document).ready(function(){jQuery('.toggle').click(function(){if(jQuery('.submenu').is(":hidden"))
+{jQuery('.submenu').slideDown("fast");}else{jQuery('.submenu').slideUp("fast");}
+return false;});});
+//]]>
